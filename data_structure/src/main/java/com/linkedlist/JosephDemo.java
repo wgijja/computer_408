df3a2f85c8d843d5c47812cea71639b56e80d671
范剑虹 <jianhong.fan@juewei.cn>
2022-05-25 10:28:20 +0800

    约瑟夫问题-取出的顺序

--- data_structure/src/main/java/com/linkedlist/JosephDemo.java
+++ data_structure/src/main/java/com/linkedlist/JosephDemo.java
@@ -9,8 +9,10 @@ public class JosephDemo {
 
     public static void main(String[] args) {
         Joseph joseph = new Joseph();
-        joseph.addLast(33);
+        joseph.addLast(10);
         joseph.list();
+        System.out.println("取出的顺序是：");
+        joseph.getOutOrder(2, 3, 10);
     }
 }
 
@@ -55,9 +57,49 @@ class Joseph {
         while (true) {
             System.out.printf("节点编号为：%d\n", cur.no);
             cur = cur.next;
-            if (cur.no == 1){
+            if (cur.no == 1) {
                 break;
             }
+        }
+    }
+
+    /**
+     * 根据用户输入，获取约瑟夫问题的出链表的节点顺序
+     *
+     * @param startNum  开始的位置
+     * @param eachCount 每次数的个数
+     * @param size
+     */
+    public void getOutOrder(int startNum, int eachCount, int size) {
+        if (first == null || startNum < 1 || startNum > size) {
+            System.out.println("参数不正确！");
+            return;
+        }
+        //创建另一个辅助变量
+        JosephNode helper = first;
+        //移动到链表的最后
+        while (helper.next != first) {
+            helper = helper.next;
+        }
+        //将两个辅助变量移动到开始的位置
+        for (int i = 0; i < startNum - 1; i++) {
+            first = first.next;
+            helper = helper.next;
+        }
+        //根据指示移动多少个节点
+        while (true) {
+            //当first==helper时，即为最后一个
+            if (first == helper) {
+                System.out.printf("取出的节点是：%d", first.no);
+                break;
+            }
+            for (int i = 0; i < eachCount - 1; i++) {
+                first = first.next;
+                helper = helper.next;
+            }
+            System.out.printf("取出的节点是：%d\n", first.no);
+            first = first.next;
+            helper.next = first;
         }
     }
 }
