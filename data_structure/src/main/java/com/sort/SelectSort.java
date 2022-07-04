a42ddde85cf20e2248afe26604fca15f01b16778
leslie <wgijja@126.com>
2022-07-04 21:51:06 +0800

    选择排序

---
+++ data_structure/src/main/java/com/sort/SelectSort.java
@@ -0,0 +1,48 @@
+package com.sort;
+
+import org.apache.commons.lang3.time.StopWatch;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+public class SelectSort {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 4, 6, 1, 3, 5, 7, 9, 8};
+        selectSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        //测试一下性能，测试80000条数据执行时间 3S
+        int[] maxSize = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            maxSize[i] = (int) (Math.random() * 80000);
+        }
+        StopWatch stopWatch = StopWatch.createStarted();
+        selectSort(maxSize);
+        stopWatch.stop();
+        System.out.println("执行花费了：" + stopWatch.getTime(TimeUnit.SECONDS) + "秒");
+
+    }
+
+    /**
+     * 选择排序
+     *
+     * @param arr
+     */
+    private static void selectSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            //假定第i位是最小的。
+            int minTemp = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minTemp]) {
+                    minTemp = j;
+                }
+            }
+            if (i != minTemp) {
+                int temp = arr[i];
+                arr[i] = arr[minTemp];
+                arr[minTemp] = temp;
+            }
+        }
+    }
+}
