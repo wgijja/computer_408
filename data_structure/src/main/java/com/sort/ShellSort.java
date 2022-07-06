16e8fa8782f9493d0eb781527985c5dc8beeb4c6
leslie <wgijja@126.com>
2022-07-06 22:23:41 +0800

    希尔排序：位移法（更优）

--- data_structure/src/main/java/com/sort/ShellSort.java
+++ data_structure/src/main/java/com/sort/ShellSort.java
@@ -1,6 +1,9 @@
 package com.sort;
 
+import org.apache.commons.lang3.time.StopWatch;
+
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 public class ShellSort {
 
@@ -9,10 +12,32 @@ public class ShellSort {
         int[] arr = new int[]{2, 4, 6, 1, 3, 5, 7, 9, 8};
         shellSort(arr);
         System.out.println(Arrays.toString(arr));
+
+
+        //测试一下性能，测试80000条数据执行时间 9s
+        //  int[] maxSize = new int[80000];
+//        for (int i = 0; i < 80000; i++) {
+//            maxSize[i] = (int) (Math.random() * 80000);
+//        }
+//        StopWatch stopWatch = StopWatch.createStarted();
+//        shellSort(maxSize);
+//        stopWatch.stop();
+//        System.out.println("交换法执行花费了：" + stopWatch.getTime(TimeUnit.SECONDS) + "s");
+
+
+        //测试一下性能，测试80000条数据执行时间 9s
+        int[] maxSize2 = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            maxSize2[i] = (int) (Math.random() * 80000);
+        }
+        StopWatch stopWatch2 = StopWatch.createStarted();
+        shellSortGood(maxSize2);
+        stopWatch2.stop();
+        System.out.println("位移法执行花费了：" + stopWatch2.getTime(TimeUnit.MILLISECONDS) + "ms");
     }
 
     /**
-     * 希尔排序
+     * 希尔排序(交换式）
      *
      * @param arr
      */
@@ -27,6 +52,27 @@ public class ShellSort {
                         arr[j] = arr[j + gap];
                         arr[j + gap] = temp;
                     }
+                }
+            }
+        }
+    }
+
+    /**
+     * 希尔排序（移位法）
+     *
+     * @param arr
+     */
+    public static void shellSortGood(int[] arr) {
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                int temp = arr[j];
+                if (temp < arr[j - gap]) {
+                    while (j - gap >= 0 && temp < arr[j - gap]) {
+                        arr[j] = arr[j - gap];
+                        j -= gap;
+                    }
+                    arr[j] = temp;
                 }
             }
         }
