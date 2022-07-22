3276c135d15f02db150a2478b080c065091a96e9
范剑虹 <jianhong.fan@juewei.cn>
2022-07-22 10:11:12 +0800

    快速归并排序

--- data_structure/src/main/java/com/sort/MergeSort.java
+++ data_structure/src/main/java/com/sort/MergeSort.java
@@ -14,18 +14,17 @@ public class MergeSort {
         int[] arr = new int[]{8, 4, 5, 7, 1, 3, 6, 2};
         //归并排序需要额外的空间
         int[] temp = new int[arr.length];
-        mergeSortPractice(arr, 0, arr.length - 1, temp);
+        mergeSortPra2(arr, 0, arr.length - 1, temp);
         System.out.println(Arrays.toString(arr));
 
-        int[] maxSize = new int[80000000];
-        for (int i = 0; i < 80000000; i++) {
-            maxSize[i] = (int) (Math.random() * 80000000);
-        }
-
-        StopWatch stopWatch = StopWatch.createStarted();
-        mergeSortPractice(maxSize, 0, arr.length - 1, temp);
-        stopWatch.stop();
-        System.out.println("执行花费了：" + stopWatch.getTime(TimeUnit.MICROSECONDS) + "ms");
+        //int[] maxSize = new int[80000000];
+        //for (int i = 0; i < 80000000; i++) {
+        //    maxSize[i] = (int) (Math.random() * 80000000);
+        //}
+        //StopWatch stopWatch = StopWatch.createStarted();
+        //mergeSortPractice(maxSize, 0, arr.length - 1, temp);
+        //stopWatch.stop();
+        //System.out.println("执行花费了：" + stopWatch.getTime(TimeUnit.MICROSECONDS) + "ms");
     }
 
 
@@ -134,7 +133,6 @@ public class MergeSort {
             j++;
             t++;
         }
-
         //把数据从temp拷贝到原数组
         int tempLeft = left;
         t = 0;
@@ -143,9 +141,52 @@ public class MergeSort {
             tempLeft++;
             t++;
         }
+    }
 
+    private static void mergeSortPra2(int[] arr, int left, int right, int[] temp) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSortPra2(arr, left, mid, temp);
+            mergeSortPra2(arr, mid + 1, right, temp);
+            mergePra2(arr, left, mid, right, temp);
+        }
 
     }
 
+    private static void mergePra2(int[] arr, int left, int mid, int right, int[] temp) {
+        int i = left;
+        int j = mid + 1;
+        int t = 0;
+        //把左右其中一边全部加到中转表
+        while (i <= mid && j <= right) {
+            if (arr[i] < arr[j]) {
+                temp[t] = arr[i];
+                i++;
+            } else {
+                temp[t] = arr[j];
+                j++;
+            }
+            t++;
+        }
+        //把剩下的加入中转表
+        while (i <= mid) {
+            temp[t] = arr[i];
+            i++;
+            t++;
+        }
+        while (j <= right) {
+            temp[t] = arr[j];
+            j++;
+            t++;
+        }
+        //把中转数组中的数据拷贝回原数组
+        t = 0;
+        int tempLeft = left;
+        while (tempLeft <= right) {
+            arr[tempLeft] = temp[t];
+            tempLeft++;
+            t++;
+        }
+    }
 
 }
